d98b62cdd61a9b6295cc0a42b5146939db369fb3
akagenoin <136881161+akagenoin@users.noreply.github.com>
2024-09-29 14:29:38 +0900

    Update ros2-foxy.bash

--- ros2-foxy.bash
+++ ros2-foxy.bash
@@ -1,9 +1,18 @@
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
+sudo apt update
+sudo apt install curl gnupg2 lsb-release
+
+sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key  -o /usr/share/keyrings/ros-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(source /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
+
+echo "install ros2"
 
 sudo apt update
+sudo apt install ros-foxy-desktop
 
-sudo apt upgrade -y
+cd echo "source /opt/ros/foxy/setup.bash" >> ~/.bashrc
+source ~/.bashrc
 
-sudo apt install ros-foxy-ros-base python3-argcomplete
+sudo apt install python3-colcon-common-extensions python3-rosdep2
 
-sudo apt install ros-dev-tools
+sudo rosdep init
+rosdep update
